a62dc5b3b69259633769acc4bd4e35443a77a9cc
Pascal Simon <pascaltobiassimon@gmail.com>
2021-03-20 13:50:37 +0100

    ♻️  Generalize navigation behaviour

--- behaviours/navigation.py
+++ behaviours/navigation.py
@@ -1,3 +1,4 @@
+from typing import Tuple
 import numpy as np
 from engine.behaviour import Behaviour
 from esper import World
@@ -6,18 +7,18 @@ from simulation.components.position import Position
 from simulation.components.velocity import Velocity
 
 
-class BehaviourFollow(Behaviour):
+class BehaviourGoto(Behaviour):
 
-    def __init__(self, target: int, distance: int = 0):
-        self.distance = distance
-        self.target = target
-        self.valid = True
+    def __init__(self, target_x: int, target_y: int):
+        self.__target_x = target_x
+        self.__target_y = target_y
+        self.__valid = True
 
     def priority(self):
         return super().priority()
 
     def valid(self):
-        return self.valid
+        return self.__valid
 
     def execute(self, world: World, entity: int):
 
@@ -28,23 +29,66 @@ class BehaviourFollow(Behaviour):
             self_position = world.component_for_entity(entity, Position)
             self_velocity = world.component_for_entity(entity, Velocity)
             self_acceleration = world.component_for_entity(entity, Acceleration)
-
-            target_position = world.component_for_entity(self.target, Position)
-            target_velocity = world.component_for_entity(self.target, Velocity)
+            self.__valid = True
         except KeyError:
-            self.valid = False
+            self.__valid = False
             return
 
-        # Prepare data as vectors
         v_self_position = np.array([self_position.x, self_position.y])
         v_self_velocity = np.array([self_velocity.x, self_velocity.y])
 
+        v_navigation_target = np.array(self._get_target_point(world, entity))
+
+        # What has to actually happen to get to the navigation target?
+        v_course_correction = v_navigation_target - (v_self_position + v_self_velocity)
+
+        #Enfore maximum acceleration
+        v_course_correction = v_course_correction / np.linalg.norm(v_course_correction)
+        v_course_correction *= self._get_max_acceleration()
+
+        #Accelerate to correct course
+        self_acceleration.x = v_course_correction[0]
+        self_acceleration.y = v_course_correction[1]
+
+    def _get_target_point(self) -> Tuple[int, int]:
+        return (self.__target_x, self.__target_y)
+
+    def _get_max_acceleration(self) -> float:
+        # FIXME The maximum acceleration of entities should be a system.
+        return 0.001
+
+class BehaviourFollow(BehaviourGoto):
+
+    def __init__(self, target: int, distance: int = 0):
+        self.__distance = distance
+        self.__target = target
+        self.__valid = True
+
+    def priority(self):
+        return super().priority()
+
+    def __valid(self):
+        return self.__valid
+
+    def _get_target_point(self, world: World, entity: int) -> Tuple[int, int]:
+
+        # Prepare data
+        try:
+            self_position = world.component_for_entity(entity, Position)
+            target_position = world.component_for_entity(self.__target, Position)
+            target_velocity = world.component_for_entity(self.__target, Velocity)
+            self.__valid = super().valid()
+        except KeyError:
+            self.__valid = False
+            return
+
+        v_self_position = np.array([self_position.x, self_position.y])
         v_target_position = np.array([target_position.x, target_position.y])
         v_target_velocity = np.array([target_velocity.x, target_velocity.y])
 
         # Calculate navigation target
         v_navigation_target = v_target_position
-        if self.distance > 0:
+        if self.__distance > 0:
             # Adjust for follow distance and go there
             target_velocity_size = np.linalg.norm(v_target_velocity)
             if target_velocity_size > 0:
@@ -52,18 +96,10 @@ class BehaviourFollow(Behaviour):
             else:
                 v_follow_velocity = v_target_position - v_self_position
                 v_follow_velocity = v_follow_velocity / np.linalg.norm(v_follow_velocity)
-            v_follow_velocity *= -1 * self.distance
+            v_follow_velocity *= -1 * self.__distance
             v_navigation_target = v_target_position + v_follow_velocity
         else:
             # Navigate right to where the target will be next tick
             v_navigation_target = v_target_position + v_target_velocity
 
-        # What has to actually happen to get to the navigation target?
-        v_course_correction = v_navigation_target - (v_self_position + v_self_velocity)
-
-        # FIXME Enforce some maximum acceleration. The maximum acceleration of entities should be a system.
-        v_course_correction = v_course_correction / np.linalg.norm(v_course_correction)
-        v_course_correction *= 0.001
-
-        self_acceleration.x = v_course_correction[0]
-        self_acceleration.y = v_course_correction[1]
+        return (v_navigation_target[0], v_navigation_target[1])
